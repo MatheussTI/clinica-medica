49d52834011921f07b06caad1757767dd95e674b
Matheus A. Rangel <135743424+MatheussTI@users.noreply.github.com>
2024-08-13 22:22:15 -0300

    Update AppClinica.java

--- AppClinica.java
+++ AppClinica.java
@@ -7,8 +7,10 @@ import lista.VetPaciente;
 public class AppClinica {
 
 	Scanner sc = new Scanner(System.in);
+	boolean continuar = true;
+	static VetPaciente vetor = new VetPaciente();
 
-	public static void main(String[] args) {
+	public void menu() {
 		
 		int opcao;
 		
@@ -23,34 +25,41 @@ public class AppClinica {
 			opcao = sc.nextInt();
 			sc.nextLine();
 			
-				switch(opcao) {
-					case 1:
-						
-						cadastrarPaciente();
-						break;
-					
-					case 2:
-						
-						consultar();
-						break;
-					case 3:
-					
-					case 4:
-					
-					default:
-						System.out.println("Insira uma opção válida!: ");
-				}
+            switch (opcao) {
+            case 1:
+            	cadastrarPaciente(sc);
+                break;
+
+            case 2:
+                this.consultar(sc);
+                break;
+
+            case 3:
+                this.imprimirCadastro();
+                break;
+
+            case 4:
+                System.out.println("Encerrando sistema...");
+                sc.close();
+                continuar = false;
+                break;
+
+            default:
+                System.out.println("Opção inválida! Por favor, tente novamente.");
+        }
+    
+
 			
 		}while(opcao != 4);
 			
 		}
 	
-	private static void cadastrarPaciente() {
+	private static void cadastrarPaciente(Scanner sc) {
 		System.out.println("Insira os seguintes campos:/nNome do paciente:");
 		String nome = sc.nextLine();
 		
 		System.out.println("CPF:");
-		String cpf = sc.nextLine();
+		String cpf = sc.nextLine();	
 		
 		System.out.println("Data de Nascimento:");
 		String DataNascimento = sc.nextLine();
@@ -59,14 +68,52 @@ public class AppClinica {
 		String Historico = sc.nextLine();
 	
 		Paciente paciente = new Paciente(nome, cpf, DataNascimento, Historico);
+		int resultado = vetor.insere(paciente);
+		
+		if(resultado == -1) {
+			System.out.println("ERRO: A lista de pacientes se encontra cheia, tente novamente mais tarde.");
+			
+		}else if (resultado == -2) {
+            System.out.println("Erro: Já existe um paciente com esse CPF cadastrado.");
+		
+		}else if (resultado == 0) {
+            System.out.println("\nUsuário cadastrado com sucesso!\n");
+          
+		}
 		
 	}
 	
-	private static void consultar() {
+	private static void consultar(Scanner sc) {
 		System.out.println("Por favor, insira o CPF do paciente procurado: ");
-		String cpf = sc.next();
+		String cpf = sc.nextLine();
+		int pos = vetor.pesquisa(cpf);
+			
+        if (pos != -1) {
+            Paciente paciente = vetor.getPaciente(pos);
+            if (paciente != null) {
+                System.out.println("\n" + paciente.getNome() 
+                + " | CPF: "  + paciente.getCPF() 
+                + " | Data de Nascimento: " + paciente.getDataNascimento()
+                + "Histórico: " + paciente.getHistorico());
+            }
+        } else {
+            System.out.println("Paciente não encontrado.\n");
+        }
+    }
+	
+	 public void imprimirCadastro() {
+	        System.out.println("Imprimindo dados de todos os pacientes:");
+	        for (int i = 0; i < vetor.getNElem(); i++) {
+	            Paciente paciente = vetor.getPaciente(i);
+	            if (paciente != null) {
+	                System.out.println("Nome: " + paciente.getNome() + 
+	                                   "\nCPF: " + paciente.getCPF() + 
+	                                   "\nData de Nascimento: " + paciente.getDataNascimento() + 
+	                                   "\n");
+	            }
 		
-		//...
+		
 	}
 	
+}
 }
